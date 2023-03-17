6afaa4705ba051ef25bc9a89ceb5ec2519a7e4f5
tr43om <tr43om@gmail.com>
2023-03-17 14:58:36 +0600

    injected analytics

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import preprocess from 'svelte-preprocess';
-import path from 'path';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
